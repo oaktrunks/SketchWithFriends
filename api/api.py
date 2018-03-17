6e791ac3e47842802dda42d6eb8a2192d91183cd
unknown <TomeiDLT@gmail.com>
2018-03-16 21:48:13 -0500

    Got front end talking to API

--- api/api.py
+++ api/api.py
@@ -1,10 +1,30 @@
-from flask import Flask
+from flask import Flask, request
+from pymongo import MongoClient
+import json
+from flask_cors import CORS
+
+
+# Create a link to our mongo database
+client = MongoClient()
 
 app = Flask(__name__)
+CORS(app)
 
-@app.route('/submitDrawing')
+@app.route('/hello')
 def mainScreen():
     return "Hello, World!"
+
+@app.route('/sendDrawing', methods=['POST'])
+def sendDrawing():
+    #Store the drawing in mongodb
+    #for k in request.form.items():
+    #return request.get_json(force=True)
+    # data in string format and you have to parse into dictionary
+    #data = request.data
+    #dataDict = json.loads(data)
+    return request.form['paths']
+    
+        
 
 if __name__ == '__main__':
     app.run(debug=True)
