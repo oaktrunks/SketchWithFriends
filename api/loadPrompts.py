b8134593d73a2bdb7ef750eb94dfd321d8bbeae5
oaktrunks <TomeiDLT@gmail.com>
2018-04-12 13:15:41 -0500

    Uploaded prompts.txt and created loadPrompts.py

---
+++ api/loadPrompts.py
@@ -0,0 +1,25 @@
+'''
+This script reads the prompts.txt and puts the prompts into
+our database after wiping it of any previous promts
+'''
+
+from pymongo import MongoClient
+import json
+
+client = MongoClient()
+db = client['sketchwithfriends']
+
+try:
+    file = open("prompts.txt", "r")
+    document = {}
+    promptList = []
+    for word in file:
+        promptList.append(word)
+    document["promptList"] = promptList
+    result = db["prompts"].insert_one(document)
+    if result.acknowledged is True :
+        print("success:True")
+    else:
+        print("success:False")
+except:
+    print("Error: file not read")
